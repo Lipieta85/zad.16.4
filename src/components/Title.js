b0f3569cddafc731c3723cfaccb98d17f87bedd3
Rafał Lipieta <cwgflojdid@wp.pl>
2018-07-26 14:22:38 +0200

    Add Title.js and Title.css

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Title.css';
+
+
+const Title = props => {
+    return <div className={style.Title}>
+           <h1>{props.title}</h1>
+           <p>Tasks Number ToDo: {props.number}</p>
+           </div>
+}
+
+export default Title;
